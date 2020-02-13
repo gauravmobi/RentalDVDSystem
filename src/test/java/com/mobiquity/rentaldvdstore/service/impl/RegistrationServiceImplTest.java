a53ee702bf5a08a72a6bc97164f5045b9696b07d
Dipak Balsantosh <dbalsantosh@mobiquityinc.com>
2020-02-13 17:37:34 +0530

    Changes in Registration service impl test

--- src/test/java/com/mobiquity/rentaldvdstore/service/impl/RegistrationServiceImplTest.java
+++ src/test/java/com/mobiquity/rentaldvdstore/service/impl/RegistrationServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.mobiquity.rentaldvdstore.service.impl;
 
+import com.mobiquity.rentaldvdstore.dao.LoginDao;
 import com.mobiquity.rentaldvdstore.dao.RegistrationDao;
 import com.mobiquity.rentaldvdstore.pojo.Address;
 import com.mobiquity.rentaldvdstore.pojo.City;
 import com.mobiquity.rentaldvdstore.pojo.Country;
 import com.mobiquity.rentaldvdstore.pojo.Customer;
+import com.mobiquity.rentaldvdstore.service.impl.LoginServiceImpl;
+import com.mobiquity.rentaldvdstore.service.impl.RegistrationServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,33 +24,71 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration
 public class RegistrationServiceImplTest {
     RegistrationServiceImpl service = new RegistrationServiceImpl();
-    Customer customer;
-
     @Mock
     RegistrationDao registrationDao;
+    Customer customer;
+    Address address;
+    City city;
+    Country country;
 
     @Before
     public void setup() {
         service.setRegistrationDao(registrationDao);
     }
 
+    @Before
+    public void prepare() {
+        customer = new Customer();
+        address = new Address();
+        city = new City();
+        country = new Country();
+    }
+
     @Test
     public void testSuccessfullyRegistration() {
-        customer =  new Customer(101, "Gaurav", "Sonawane", "g@gmail.com", true, "gaurav",
-                    new Address(11, "Pune", "Pune",
-                    new City(01, "Pune",
-                    new Country(111, "India")), 111111, "Pune"));
-        Mockito.when(registrationDao.userRegistration(customer)).thenReturn("Success");
+        customer.setCustomer_id(101);
+        customer.setLast_name("Dipak");
+        customer.setLast_name("Balsantosh");
+        customer.setEmail("dipak@gmail.com");
+        customer.setActive(true);
+        customer.setPassword("dipak");
+        address.setAddress_id(11);
+        address.setAddress("a/p Laxmi chowk 32 Shirala");
+        address.setDistrict("Sangli");
+        address.setPostal_code(415408);
+        address.setLocation("Shirala");
+        city.setCity_id(111);
+        city.setCity("32 Shirala");
+        country.setCountry_id(1);
+        country.setCountry("India");
+        Mockito.when(registrationDao.userRegistration(customer)).thenReturn("Registration Success");
         assertEquals("Registration Success", service.userRegistration(customer));
     }
 
     @Test
-    public void testFailRegistration() {
-        customer =  new Customer(101, "Gaurav", "Sonawane", "g@gmail.com", true, "gaurav",
-                    new Address(11, "Pune", "Pune",
-                    new City(01, "Pune",
-                    new Country(111, "India")), 111111, "Pune"));
+    public void testFailRegistrationWithException() {
+        customer.setCustomer_id(101);
+        customer.setLast_name("Dipak");
+        customer.setLast_name("Balsantosh");
+        customer.setEmail("dipak@gmail.com");
+        customer.setActive(true);
+        customer.setPassword("dipak");
+        address.setAddress_id(11);
+        address.setAddress("a/p Laxmi chowk 32 Shirala");
+        address.setDistrict("Sangli");
+        address.setPostal_code(415408);
+        address.setLocation("Shirala");
+        city.setCity_id(111);
+        city.setCity("32 Shirala");
+        country.setCountry_id(1);
+        country.setCountry("India");
         Mockito.when(registrationDao.userRegistration(customer)).thenThrow(RuntimeException.class);
         assertEquals("Registration Failed", service.userRegistration(customer));
     }
+    @Test
+    public void testFailRegistrationWithEmpty() {
+        Mockito.when(registrationDao.userRegistration(customer)).thenReturn(null);
+        assertEquals("Registration Failed", service.userRegistration(customer));
+    }
+
 }
