2feafdda33bef3df97be73526c77cd5554d525a6
nisargatmobi <nturke@mobiquityinc.com>
2020-03-30 10:53:54 +0530

    Added test cases for customer controller.

--- src/test/java/com/mobiquity/rentaldvdstore/Controller/CustomerControllerTest.java
+++ src/test/java/com/mobiquity/rentaldvdstore/Controller/CustomerControllerTest.java
@@ -1,47 +1,39 @@
 package com.mobiquity.rentaldvdstore.Controller;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mobiquity.rentaldvdstore.controller.CustomerController;
 import com.mobiquity.rentaldvdstore.pojo.Address;
-import com.mobiquity.rentaldvdstore.pojo.City;
-import com.mobiquity.rentaldvdstore.pojo.Country;
 import com.mobiquity.rentaldvdstore.pojo.Customer;
+import com.mobiquity.rentaldvdstore.service.DeleteCustomerService;
 import com.mobiquity.rentaldvdstore.service.ListingService;
+import com.mobiquity.rentaldvdstore.service.LoginService;
 import com.mobiquity.rentaldvdstore.service.RegistrationService;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.any;
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.sort;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(com.mobiquity.rentaldvdstore.controller.CustomerController.class)
 public class CustomerControllerTest {
 
-    @InjectMocks
-    com.mobiquity.rentaldvdstore.controller.CustomerController customerController;
-    @Mock
-    RegistrationService registrationService;
-    @Mock
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
     ListingService listingService;
-
+    @MockBean
+    LoginService loginService;
+    @MockBean
+    RegistrationService registrationService;
+    @MockBean
+    DeleteCustomerService deleteCustomerService;
 
     private Customer getCustomerObject(Long id, String fname, String lname, String email, Boolean active, String password,
                                        String mobno, Address address) {
@@ -57,112 +49,153 @@ public class CustomerControllerTest {
         return customer;
     }
 
-    private Address getAddressObject(int addressid, String add, String district, int postalcode, String location,
-                                     City city) {
-        Address address = new Address();
-        address.setAddressId(addressid);
-        address.setAddress(add);
-        address.setDistrict(district);
-        address.setPostalCode(postalcode);
-        address.setLocation(location);
-        address.setCity(city);
-        return address;
-    }
-
-    private City getCityObject(int cityid, String cityname, Country country) {
-        City city = new City();
-        city.setCityId(cityid);
-        city.setCity(cityname);
-        city.setCountry(country);
-        return city;
-    }
-
-    private Country getCountryObject(int countryid, String countryname) {
-        Country country = new Country();
-        country.setCountryId(countryid);
-        country.setCountry(countryname);
-        return country;
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     @Test
-    public void testForRegisterCustomer() {
+    public void testGetAllCustomers() throws Exception{
 
-        Country country = getCountryObject(1, "India");
-        City city = getCityObject(111, "Pune", country);
-        Address address = getAddressObject(3, "a/p Laxmi chowk 32 Shirala", "Sangli",
-                415408, "Shirala", city);
-        Customer customer = getCustomerObject(1213L, "dipak", "b", "abc@gmail.com",
-                true, "abcW$1", "+919999999999", address);
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-        System.out.println(customer);
-        Mockito.when(registrationService.userRegistration(customer)).thenReturn("Customer Registered Successfully");
-
-        ResponseEntity<String> responseEntity = customerController.customerRegistration(customer);
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        mvc.perform(MockMvcRequestBuilders
+                .get("/customers")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testForGetAllCustomers() {
-        Country country = getCountryObject(1, "India");
-        City city = getCityObject(111, "Pune", country);
-        Address address = getAddressObject(3, "a/p Laxmi chowk 32 Shirala", "Sangli",
-                415408, "Shirala", city);
-        Customer customer1 = getCustomerObject(1213L, "dipak", "b", "abc@gmail.com",
-                true, "abcW$1", "+919999999999", address);
-        Country country2 = getCountryObject(1, "India");
-        City city2 = getCityObject(111, "Pune", country);
-        Address address2 = getAddressObject(3, "a/p Laxmi chowk 32 Shirala", "Sangli",
-                415408, "Shirala", city);
-        Customer customer2 = getCustomerObject(1213L, "dipak", "b", "abc@gmail.com",
-                true, "abcW$1", "+919999999999", address);
+    public void testGetAllCustomersWithIncorrectAPI() throws Exception{
 
-        List listOfAllCustomers = new ArrayList();
-        listOfAllCustomers.add(customer1);
-        listOfAllCustomers.add(customer2);
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-        Mockito.when(listingService.getAllCustomers()).thenReturn(listOfAllCustomers);
-        ResponseEntity<List<Customer>> responseEntity = customerController.getAllCustomers();
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        mvc.perform(MockMvcRequestBuilders
+                .get("/customer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 
     @Test
-    public void testForGetAllActiveCustomers() {
+    public void testGetAllActiveCustomers() throws Exception{
 
-        Country country = getCountryObject(1, "India");
-        City city = getCityObject(111, "Pune", country);
-        Address address = getAddressObject(3, "a/p Laxmi chowk 32 Shirala", "Sangli",
-                415408, "Shirala", city);
-        Customer customer1 = getCustomerObject(1213L, "dipak", "b", "abc@gmail.com",
-                true, "abcW$1", "+919999999999", address);
-
-        Country country2 = getCountryObject(1, "India");
-        City city2 = getCityObject(111, "Pune", country);
-        Address address2 = getAddressObject(3, "a/p Laxmi chowk 32 Shirala", "Sangli",
-                415408, "Shirala", city);
-        Customer customer2 = getCustomerObject(1213L, "dipak", "b", "abc@gmail.com", true, "abcW$1", "+919999999999", address);
-
-
-        List listOfAllActiveCustomers = new ArrayList();
-        listOfAllActiveCustomers.add(customer1);
-        listOfAllActiveCustomers.add(customer2);
-
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        Mockito.when(listingService.getListOfAllActiveCustomers()).thenReturn(listOfAllActiveCustomers);
-        ResponseEntity<List<Customer>> responseEntity = customerController.getAllActiveCustomers();
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        mvc.perform(MockMvcRequestBuilders
+                .get("/active")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
+    @Test
+    public void testGetAllActiveCustomersWithIncorrectAPI() throws Exception{
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testCustomerLogin() throws Exception{
+        String email="email";
+        String password="password";
+        mvc.perform(MockMvcRequestBuilders
+                .get("/login")
+                .param("password",password)
+                .param("email",email)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testCustomerLoginWithIncorectAPI() throws Exception{
+        String email="email";
+        String password="password";
+        mvc.perform(MockMvcRequestBuilders
+                .get("/")
+                .param("password",password)
+                .param("email",email)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testCustomerLoginWithIncorrectParameterNames() throws Exception{
+        String email="email";
+        String password="password";
+        mvc.perform(MockMvcRequestBuilders
+                .get("/login")
+                .param("Password",password)
+                .param("Email",email)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testRegisterCustomer() throws Exception{
+
+        Customer customer = getCustomerObject(1213L, "nisarg", "turke", "nisarg@mail.com",
+                true, "abcW$1", "+919999999999", null);
+
+        mvc.perform( MockMvcRequestBuilders
+                .post("/registration")
+                .content(asJsonString(customer))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testRegisterCustomerWithIncorrectAPI() throws Exception{
+
+        Customer customer = getCustomerObject(1213L, "nisarg", "turke", "nisarg@mail.com",
+                true, "abcW$1", "+919999999999", null);
+
+        mvc.perform( MockMvcRequestBuilders
+                .post("/")
+                .content(asJsonString(customer))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+
+    @Test
+    public void testRegisterCustomerWithNullObject() throws Exception{
+
+        mvc.perform( MockMvcRequestBuilders
+                .post("/")
+                .content(asJsonString(null))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    public void testDeleteCustomer() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/delete/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteCustomerWithIncorrectId() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/delete/1L")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testDeleteCustomerWithIncorrectAPI() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .delete("")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
 
 
 }
-//
-//
-//
